4bf7bba7f2d94525d255fb235a11e58d90bf5dd3
Jimmy <rivendinner@gmail.com>
2024-11-30 04:03:21 +0800

    backup

---
+++ include/utils/validator.hpp
@@ -0,0 +1,12 @@
+#include <string>
+
+template <typename T>
+std::string validate(T obj) {
+    // const auto view = rfl::to_view(obj);
+
+    // if (*view.get<"name">() == "Maggie"){
+    //   return "name not empty";
+    // }
+
+    return "";
+}
--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <type_traits>
 #include <utils/parser.hpp>
+#include <utils/validator.hpp>
+#include "rfl.hpp"
 
 using json = nlohmann::json;
 
@@ -13,8 +15,11 @@ int main() {
     CROW_ROUTE(app, "/")
         .methods("POST"_method)
         ([](const crow::request& req) {
+            // using stringNotLongerThan10 = rfl::Validator<std::string, rfl::Maximum<10>>;
+            // using Value = rfl::Validator<int, rfl::Minimum<0>, rfl::Maximum<10>>;
             struct Request {
                 std::string id;  
+                // stringNotLongerThan10 name;
                 std::string name;
                 struct Nested {
                     int value;
@@ -27,12 +32,26 @@ int main() {
                 return crow::response(400, error);
             }
             
+            // if (std::string err = validate(*result); err != ""){
+            //     return crow::response(400, err);
+            // };
+
+            const auto view = rfl::to_view(*result);
+            view.apply([](const auto& f) {
+                if (f.name() == "id"){
+                    if (rfl::json::write(*f.value()) == "\"gg\""){
+                        std::cout << f.name() << ": " << rfl::json::write(*f.value()) << std::endl;
+                    }
+                }
+            });
+            // *rfl::get<"name">(view) = "Maggie";
+            // *view.get<"name">() = "Maggie";
+
+
             // Print the Nested.value field
-            std::cout << "Parsed Request: id=" << result->id 
-                      << ", name=" << result->name  
-                      << ", nested.value=" << result->nested.value << std::endl;
-
-
+            // std::cout << "Parsed Request: id=" << result->id 
+            //           << ", name=" << result->name 
+            //           << ", nested.value=" << result->nested.value << std::endl;
 
             return crow::response(200, "Request successfully processed");
         });
